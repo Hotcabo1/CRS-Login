ac9d1aac19294dada0bf9bdd852a5ae69349a3bb
Carlos <carlosceravargas@gmail.com>
2021-07-17 13:29:44 -0500

    Revert "Revert "avances 1""
    This reverts commit 6108119835609ac5b37d0363331db1ed8455af1e.

---
+++ resources/views/livewire/Starpage/init.php
@@ -0,0 +1,45 @@
+<div class="row sales layout-top-spacing" >
+    <div class="col-sm-12">
+        <div class="widget widget-chart-one">
+            <div class="widget-heading">
+                <h4 class="card-title">
+                    <p>Component | PageTitle</p>
+                </h4>
+                <ul class="tabs tob-pills">
+                    <li>
+                        <a href="javascript:void(0)"  class="tabmenu bg-dark" data-toggle="modal" data-target="#theModal"></a>
+                    </li>
+                </ul>
+            </div>
+            Search
+
+            <div class="widget-content">
+
+                <div class="tabla-responisvo">
+                    <table class="table table-botfrtrf table striped mt-1">
+                        <thead class="text-white" style="background: #383F5C;">
+                            <tr>
+                                <th class="table-th text-white">DESCRIPCION</th>
+                                <th class="table-th text-white">DESCRIPCION</th>
+                                <th class="table-th text-white">DESCRIPCION</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+            </div>
+
+
+
+ <div class="wiget-<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>"></div>
+        </div>
+    </div>
+</div>
